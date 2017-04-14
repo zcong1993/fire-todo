70c8473846cbebcc381bbd6df3bc848f87f2e5d8
zcong1993 <1005997919@qq.com>
2017-04-14 15:09:19 +0800

    fix auth error in surge.sh 👯

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,6 @@
 export const firebaseConfig = {
   apiKey: 'AIzaSyDXqdddlbuqtNjPFiwYi5qgtAnhCD9KDpQ',
   authDomain: 'fire-todo-87370.firebaseapp.com',
-  // authDomain: 'vue-todos.surge.sh',
-  // authDomain: 'vue-todos.zcong.moe',
   databaseURL: 'https://fire-todo-87370.firebaseio.com',
   storageBucket: 'fire-todo-87370.appspot.com'
 }
